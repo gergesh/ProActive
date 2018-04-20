946767fcc2ef6112f2c862807a6ebc46dd504066
Yoav Shai <gergesh@gmail.com>
2018-04-20 21:00:59 +0300

    Add Solid Explorer

--- java/men/hasha/proactive/ProActive.java
+++ java/men/hasha/proactive/ProActive.java
@@ -8,15 +8,22 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class ProActive implements IXposedHookLoadPackage {
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) {
 
-		// Shuttle hook
+        // Shuttle
         if (lpparam.packageName.equals("another.music.player")) {
             findAndHookMethod("com.simplecity.amp_library.ShuttleApplication",
                     lpparam.classLoader, "d", XC_MethodReplacement.returnConstant(true));
         }
 
-		else if (lpparam.packageName.equals("com.kabouzeid.gramophone")) {
-			findAndHookMethod("com.kabouzeid.gramophone.App", lpparam.classLoader,
-					"isProVersion", XC_MethodReplacement.returnConstant(true));
-		}
+	// Phonograph
+	else if (lpparam.packageName.equals("com.kabouzeid.gramophone")) {
+	    findAndHookMethod("com.kabouzeid.gramophone.App", lpparam.classLoader,
+	            "isProVersion", XC_MethodReplacement.returnConstant(true));
+	}
+
+	// Solid Explorer
+	else if (lpparam.packageName.equals("pl.solidexplorer2")) {
+	    findAndHookMethod("pl.solidexplorer.SELicenseManager", lpparam.classLoader,
+	            "checkUnlockerLicense", XC_MethodReplacement.returnConstant(true));
+	}
     }
 }
